a7c6e72b05076800ced260c79dde16c6eac98ce0
Vantruong205 <93271920+Vantruong205@users.noreply.github.com>
2026-03-10 17:10:26 +0000

    vocab_list_screen.dart

---
+++ lib/screens/vocab_list_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class VocabListScreen extends StatelessWidget {
+  final int grade;
+  final int unit;
+
+  const VocabListScreen({super.key, required this.grade, required this.unit});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, String>> words = [
+      {"en": "hello", "vi": "xin chào"},
+      {"en": "teacher", "vi": "giáo viên"},
+      {"en": "student", "vi": "học sinh"},
+      {"en": "school", "vi": "trường học"},
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lớp $grade - Unit $unit"),
+        backgroundColor: Colors.blue,
+      ),
+
+      body: ListView.builder(
+        itemCount: words.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.volume_up),
+            title: Text(words[index]["en"]!),
+            subtitle: Text(words[index]["vi"]!),
+          );
+        },
+      ),
+    );
+  }
+}
