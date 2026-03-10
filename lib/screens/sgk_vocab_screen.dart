c9e6cc996d77475d00012ec3369344157580d28f
Vantruong205 <93271920+Vantruong205@users.noreply.github.com>
2026-03-10 17:08:13 +0000

    sgk_vocab_screen.dart

---
+++ lib/screens/sgk_vocab_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'unit_screen.dart'; // thêm dòng này
+
+class SGKVocabScreen extends StatelessWidget {
+  final String title;
+  final int startClass;
+
+  const SGKVocabScreen({
+    super.key,
+    required this.title,
+    required this.startClass,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<int> grades = List.generate(13 - startClass, (i) => i + startClass);
+
+    return Scaffold(
+      appBar: AppBar(title: Text(title), backgroundColor: Colors.blue),
+      body: ListView.builder(
+        itemCount: grades.length,
+        itemBuilder: (context, index) {
+          int grade = grades[index];
+
+          return Card(
+            margin: const EdgeInsets.all(10),
+            child: ListTile(
+              leading: const Icon(Icons.school),
+              title: Text("Lớp $grade"),
+              subtitle: const Text("Hoàn thành 0%"),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  /// mở danh sách Unit của lớp
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => UnitScreen(grade: grade)),
+                  );
+                },
+                child: const Text("Luyện tập"),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
